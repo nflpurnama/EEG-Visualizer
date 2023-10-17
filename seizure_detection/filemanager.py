1c13e38a4b72996de8fbdb9c8c06b4424cd73713
napurnama <purnama.naufal.adli@gmail.com>
2023-10-17 22:05:50 +0700

    [dev] filemanager

---
+++ seizure_detection/filemanager.py
@@ -0,0 +1,11 @@
+from tempfile import NamedTemporaryFile
+from werkzeug.datastructures import FileStorage
+
+def retrieveEdf(file: FileStorage):
+    bytes = file.read()
+
+    with NamedTemporaryFile(delete=False, suffix=".edf") as temp_file:
+        temp_file.write(bytes)
+        temp_file_path = temp_file.name
+
+        return temp_file_path
